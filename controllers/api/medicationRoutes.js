f9f05d5497e5d6702223394b402412decf1ef135
floconno <floconno@asu.edu>
2024-02-05 19:10:52 -0700

    added PUT request in med routes

--- controllers/api/medicationRoutes.js
+++ controllers/api/medicationRoutes.js
@@ -19,3 +19,29 @@ router.post('/', async (req, res) => {
 });
 
 module.exports = router;
+
+router.put('/:id', async (req,res) => {
+    try {
+        const medication = await Medication.update(
+            {
+                name: req.body.name,
+                dosage: req.body.dosage,
+                dayOfWeek: req.body.dayOfWeek,
+                inUse: req.body.inUse,  
+            },
+            {
+                where: {
+                    id: req.params.id,
+                },
+            }
+        );
+        res.status(200).json(medication);
+    }   catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
+
+
+
