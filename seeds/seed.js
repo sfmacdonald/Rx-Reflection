6291ed9a38e7e1ca63f2082a189c293aa4df29ff
Nick Smith <nick.smith.asu@gmail.com>
2024-02-05 19:28:00 -0700

    Add medication seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,12 +2,17 @@ const sequelize = require('../config/connection');
 const { User } = require('../models');
 
 const userData = require('./userData.json');
+const medicationData = require('./medicationData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   await User.bulkCreate(userData, {
     individualHooks: true,
+    returning: true,
+  });
+
+  await User.bulkCreate(medicationData, {
     returning: true,
   });
 
