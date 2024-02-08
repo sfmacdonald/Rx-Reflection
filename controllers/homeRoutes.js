2e2264385d6e2a3b25718ece2b762ce368a63b3b
Nick Smith <nick.smith.asu@gmail.com>
2024-02-08 16:38:39 -0700

    conditionally display the logout option

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../models');
+const { Patient } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
@@ -23,7 +24,11 @@ router.get('/login', (req, res) => {
 router.get('/home', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { id: req.session.user_id } });
-    console.log(userData);
+
+    const logged_in = req.session.logged_in || false;
+
+
+    res.render('homepage', { userData, logged_in });
   } catch (err) {
     res.status(400).json(err);
   }
@@ -31,7 +36,6 @@ router.get('/home', async (req, res) => {
 
 
 
-  res.render('homepage');
 })
 
 module.exports = router;
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Medication } = require('../models');
+const { User, Medication, Patient } = require('../models');
 
 const userData = require('./userData.json');
 const medicationData = require('./medicationData.json');
+const patientData = require('./patientData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -15,6 +16,10 @@ const seedDatabase = async () => {
   await Medication.bulkCreate(medicationData, {
     returning: true,
   });
+
+  await Patient.bulkCreate(patientData, {
+    returning: true,
+  })
 
   process.exit(0);
 };
