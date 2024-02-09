2c3faed4e59f1bc5372dc258bc7c82ce881183bb
floconno <floconno@asu.edu>
2024-02-08 20:58:27 -0700

    added PUT request to patient routes

--- controllers/api/patientRoutes.js
+++ controllers/api/patientRoutes.js
@@ -28,4 +28,37 @@ router.post('/', async (req, res) => {
 
 module.exports = router;
 
+router.put('/:id', async (req,res) => {
+    try {
+        const patient = await Patient.update(
+            {
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                date_of_birth: req.body.date_of_birth,
+                gender: req.body.gender,
+                height: req.body.height,
+                weight: req.body.weight,
+                email: req.body.email,
+                street: req.body.street,
+                apt_unit_number: req.body.apt_unit_number,
+                city: req.body.city,
+                state: req.body.state,
+                zip_code: req.body.zip_code,
+                phone_1: req.body.phone_1,
+                phone_2: req.body.phone_2,
+            },
+            {
+                where: {
+                    id: req.params.id,
+                },
+            }
+        );
+        res.status(200).json(patient);
+    }   catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
+
 
